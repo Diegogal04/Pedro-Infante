57737679b51f10df403286070becd050020532eb
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-08-02 14:54:39 -0500

    borre codigo de production

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('views/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'views', 'build', 'index.html'));
-    })
-}
 
 
 //pasport config
