c6c42b8478b6c4e08ece6b8f89c56adea3498840
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-08-01 19:14:06 -0500

    ya lista para deployment

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
-const passport = require('passport')
+const passport = require('passport');
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('views/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'views', 'build', 'index.html'));
+    })
+}
 
 
 //pasport config
